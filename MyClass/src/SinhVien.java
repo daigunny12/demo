1dc7a48c7071d0731b3ceee60abf6e7eb3d91735
daigunny12 <daigunny12@gmail.com>
2019-09-18 14:09:59 +0700

    sadasdasd

---
+++ MyClass/src/SinhVien.java
@@ -0,0 +1,32 @@
+
+public class SinhVien {
+
+	private String hoTen;	
+	private float diem;
+
+	public String getHoTen(){
+		return hoTen;
+	}
+	
+	public void setHoTen(String hoTen) {
+		this.hoTen = hoTen;
+	}
+	
+	public float getDiem(){
+		return diem;
+	}
+	
+	public void setDiem(float hoTen) {
+		this.diem = diem;
+	}
+	
+	public void nhap() {
+		//
+	}
+	public void xuat() {
+		//
+	}  
+	
+	}
+
+
